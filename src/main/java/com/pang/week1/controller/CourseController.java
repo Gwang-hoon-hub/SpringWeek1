b066b5684758fa000aaca3d5ff71d7cc05f9e169
panghoon <leegwanghoon95@gmail.com>
2022-02-11 22:49:38 +0900

    Service, Controller

--- src/main/java/com/pang/week1/controller/CourseController.java
+++ src/main/java/com/pang/week1/controller/CourseController.java
@@ -1,2 +1,46 @@
-package com.pang.week1.controller;public class CourseController {
+package com.pang.week1.controller;
+
+import com.pang.week1.domain.Course;
+import com.pang.week1.domain.CourseRepository;
+import com.pang.week1.model.CourseRequestDto;
+import com.pang.week1.service.CourseService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    private final CourseService courseService;
+
+    // PostMapping을 통해서, 같은 주소라도 방식이 다름을 구분합니다.
+    @PostMapping("/api/courses")
+    public Course createCourse(@RequestBody CourseRequestDto requestDto) {
+        // requestDto 는, 생성 요청을 의미합니다.
+        // 강의 정보를 만들기 위해서는 강의 제목과 튜터 이름이 필요하잖아요?
+        // 그 정보를 가져오는 녀석입니다.
+
+        // 저장하는 것은 Dto가 아니라 Course이니, Dto의 정보를 course에 담아야 합니다.
+        // 잠시 뒤 새로운 생성자를 만듭니다.
+        Course course = new Course(requestDto);
+
+        // JPA를 이용하여 DB에 저장하고, 그 결과를 반환합니다.
+        return courseRepository.save(course);
+    }
+
+    // GET 방식으로 오면 데이터를 돌려줘라라
+   @GetMapping("/api/courses")
+    public List<Course> getCourse(){
+        return courseRepository.findAll();
+    }
+
+    @PatchMapping("/api/courses/{id}")
+    public Long updateCourse(@PathVariable Long id, @RequestBody CourseRequestDto requestDto) {
+        return courseService.update(id, requestDto);
+    }
 }
