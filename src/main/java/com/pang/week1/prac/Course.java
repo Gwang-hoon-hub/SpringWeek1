3405be50c4033286ec33eba64c81ef264fdd60a3
panghoon <leegwanghoon95@gmail.com>
2022-02-11 13:54:39 +0900

    Sparta First Week Project Commit

---
+++ src/main/java/com/pang/week1/prac/Course.java
@@ -0,0 +1,46 @@
+package com.pang.week1.prac;
+
+public class Course {
+    public String title;
+    public String tutor;
+    public int days;
+
+    public Course() {
+    }
+
+    public Course(String title, String tutor, int days) {
+        this.title = title;
+        this.tutor = tutor;
+        this.days = days;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public static void main(String[] args) {
+        Course course = new Course();
+
+
+    }
+}
